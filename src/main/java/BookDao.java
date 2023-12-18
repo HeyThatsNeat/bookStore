e7d865a0e98b27d4d9a73d4e238875118c5d4710
Hunter Eckhoff <Phantazm777@verizon.net>
2023-12-18 12:51:06 -0500

    remove space between deleteBook and the parameters

--- src/main/java/BookDao.java
+++ src/main/java/BookDao.java
@@ -3,8 +3,7 @@ import java.util.List;
 public interface BookDao {
   void addBook(Book book);
   void updateBook(Book book);
-  void deleteBook (int bookId);
+  void deleteBook(int bookId);
   Book getBookById(int bookId);
-
   List<Book> getAllBooks();
 }
